391e6c48612a0251ab4f7106e512dbc85b5dfeaa
monolaurynian <monolaurynian@gmail.com>
2025-07-05 14:28:03 +0100

    new

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        christmasGreen: '#0f5132',
+        christmasRed: '#b02a37',
+      }
+    },
+  },
+  plugins: [],
+}
