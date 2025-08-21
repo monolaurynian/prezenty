ede39caebb9f75587b95481ac01b1bc9a30bf83d
monolaurynian <monolaurynian@gmail.com>
2025-08-21 22:48:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ const getAllPresents = () => {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5173;
 const HOST = process.env.HOST || '0.0.0.0';
 
 // Middleware
